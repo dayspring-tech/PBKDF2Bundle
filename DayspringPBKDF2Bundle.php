8399ed64339411b0b5d4b7fcda7b1f7c17f1311e
Matthew Denson <mdenson@dayspring-tech.com>
2013-11-08 08:34:53 -0800

    Updated for composer in Sf 2.3

--- DayspringPBKDF2Bundle.php
+++ DayspringPBKDF2Bundle.php
@@ -2,8 +2,17 @@
 
 namespace Dayspring\PBKDF2Bundle;
 
+use Dayspring\PBKDF2Bundle\DependencyInjection\Security\Factory\PBKDF2Factory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DayspringPBKDF2Bundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new PBKDF2Factory());
+    }
 }
